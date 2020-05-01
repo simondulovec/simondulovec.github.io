a020ad48a03884a9d1840d1548911b7f11d2a7d5
Simon Dulovec <simondulovec@outlook.com>
2020-05-01 12:55:41 +0200

    prvy update

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>App</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/style.css">
+		<script src="jquery/jquery-3.5.0.min.js"></script>
+		<script src="js/script.js"></script>
+	</head>
+
+	<body>
+		<main id="app">
+		</main>
+	</body>
+</html>
