67705e768add481e7ff7185a4cb21f89547222f5
Simon Dulovec <simondulovec@outlook.com>
2020-05-03 18:04:46 +0200

    added utf8_encode

--- php/edit_emp.php
+++ php/edit_emp.php
@@ -56,11 +56,11 @@ echo "<div class='mn_ctn'>
 			</div>
 			<div class='crt_emp_btns from_bottom'>
 				<button class='sml_btn ud_emp' value=".$row["id"].">
-				<img class='sml_img' src='img/confirm1.png'>
+				<img class='sml_img' src='img/confirm.png'>
 				</button>
 				
 				<button class='sml_btn bk_edit_emp'>
-				<img class='sml_img' src='img/back1.png'>
+				<img class='sml_img' src='img/back.png'>
 				</button>
 			</div>
 		</div>
@@ -74,7 +74,7 @@ echo "<div class='mn_ctn'>
 			
 			<div class='info_btn'>
 				<button class='mini_btn cfm_pp'>
-					<img class='mini_img' src='img/confirm1.png'>
+					<img class='mini_img' src='img/confirm.png'>
 				</button>
 			</div>
 		</div>
@@ -87,7 +87,7 @@ echo "<div class='mn_ctn'>
 
 			<div class='qtn_btns'>	
 				<button class='mini_btn con_pp'>
-					<img class='mini_img' src=img/confirm1.png>
+					<img class='mini_img' src=img/confirm.png>
 				</button>
 				<button class='mini_btn can_pp'>
 					<img class='mini_img' src='img/cancel.png'>
--- php/ld_emp_nm.php
+++ php/ld_emp_nm.php
@@ -6,7 +6,7 @@ $result = $conn->query($sql);
 $row = $result->fetch_assoc();
 $list = explode("|",$row["emp"]);
 echo "<div class='user_pn'>
-      <div class='name_pn'>".$list[0]."</div>
-      <div class='check_in_pn'>".$list[1]."</div>	
+      <div class='name_pn'>".utf8_encode($list[0])."</div>
+      <div class='check_in_pn'>".utf8_encode($list[1])."</div>	
       </div>";
 ?>
